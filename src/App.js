6da1d37b2a57d0b884e64c8519dcd64388e65e0d
Konstantin Vengura <konstantin.vengura@skywindtech.corp>
2022-05-22 17:36:15 +0300

    made the app dynamic using props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import Contact from "./components/Contact";
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Contact 
+      name="Amy Hunt"
+      avatar="https://randomuser.me/api/portraits/women/39.jpg"
+      online={true}
+      />
+      <Contact 
+      name="Christopher Young"
+      avatar="https://randomuser.me/api/portraits/men/90.jpg"
+      online={false}
+      />
+      <Contact 
+      name="Terra Matthews"
+      avatar="https://randomuser.me/api/portraits/women/64.jpg"
+      online={true}
+      />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,30 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import "./Contact.css";
 
-function Contact() {
+function Contact(props) {
     return (
         <div className="Contact">
             <img
                 className="avatar"
-                src="https://randomuser.me/api/portraits/women/39.jpg"
+                src={props.avatar}
                 alt=""
             />
             <div>
-                <div className="name">Amy Hunt</div>
+                <div className="name">{props.name}</div>
                 <div className="status">
-                    <div className="status-online"></div>
-                    <div className="status-text">online</div>
+                    <div className={props.online ? 'status-online' : 'status-offline'}></div>
+                    <div className="status-text">{props.online ? 'online' : 'offline'}</div>
                 </div>
             </div>
         </div>
     );
 }
+
+Contact.propTypes = {
+    name: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    online: PropTypes.bool.isRequired,
+  };
 
 export default Contact;
